c5574418e3f3c8b318b1629da08202af1b56612b
Gustavo Lopes <guhrlopes999@gmail.com>
2024-11-22 16:48:10 -0300

    projeto alura-space

--- galeria/views.py
+++ galeria/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h1>Alura Space</h1>')
+    return render(request, 'index.html')
+
+def imagem(request):
+    return render(request, 'imagem.html')
