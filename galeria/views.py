b9215c2fb4eb57b54ea128275e4309d5ccc2d27d
Gustavo Lopes <guhrlopes999@gmail.com>
2024-11-26 17:31:25 -0300

    mudanças

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'galeria/index.html')
 
 def imagem(request):
-    return render(request, 'imagem.html')
+    return render(request, 'galeria/imagem.html')
