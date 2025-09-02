7dd369f58848345cd49634d792276f451b560077
Gustavo Lopes <guhrlopes999@gmail.com>
2025-09-02 17:22:53 -0300

    Alterações realizadas

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from usuarios.forms import LoginForms, CadastroForms
+from django.contrib.auth.models import User
 
 def login(request):
   form = LoginForms()
@@ -7,4 +8,27 @@ def login(request):
 
 def cadastro(request):
   form = CadastroForms()
+
+  if request.method == 'POST':
+    form = CadastroForms(request.POST)
+    
+    if form.is_valid():
+      if form['senha'].value() != form['confirmacao_senha'].value():
+        return redirect('cadastro')
+      
+      nome = form['nome_cadastro'].value()
+      email = form['email'].value()
+      senha = form['senha'].value()
+
+      if User.objects.filter(username = nome).exists():
+        return redirect('cadastro')
+      
+      usuario = User.objects.create_user(
+      username = nome,
+      email = email,
+      password = senha
+      )
+      usuario.save()
+      return redirect('login')
+
   return render(request, 'usuarios/cadastro.html', {'form': form})
